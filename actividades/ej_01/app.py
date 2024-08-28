bfef5f71dbaa3ac60c40a45ac2e7e89021ef2d1b
Agustin Dubal <agustin.dubal06@gmail.com>
2024-08-28 16:39:48 -0300

    feat: agregar funcion para devolver estilos

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -7,5 +7,10 @@ app = Microdot() # Instanciamos la clase Microdot
 async def index(request):
     return send_file('index.html')
 
+
+@app.route('/<dir>/<file>') # Definimos la ruta con dos parámetros
+async def index(request, dir, file):
+    return send_file("/" + dir + "/" + file) # Cambiamos el nombre del archivo a enviar
+
 # Corremos el servidor creado en el puerto 80
 app.run(port=80)
