0f6bd1cf6bc8bc7040f2d411ee294b406bb26141
Agustin Dubal <agustin.dubal06@gmail.com>
2024-09-04 14:28:18 -0300

    refactor: cambiar configuracion de red

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,15 +1,10 @@
-# Configuracion inicial
-import network
-
-# Configuracion de la red
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('Cooperadora Alumnos', '')
-
-print('Conectando a la red...')
-while not sta_if.isconnected():
-    pass
-
-print('Conectado a la red')
-print(sta_if.ifconfig())
-
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('Conectando a la red...')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            pass
+    print('Red conectada:', sta_if.ifconfig())
