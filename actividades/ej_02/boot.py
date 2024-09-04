3b9f795a85ec9bbe28446d055fea202f44ac0a33
Agustin Dubal <agustin.dubal06@gmail.com>
2024-09-04 14:24:34 -0300

    feat: agregar configuración de red

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,15 @@
 # Configuracion inicial
+import network
+
+# Configuracion de la red
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('Cooperadora Alumnos', '')
+
+print('Conectando a la red...')
+while not sta_if.isconnected():
+    pass
+
+print('Conectado a la red')
+print(sta_if.ifconfig())
+
